d645be6470ae6566014f74096ad725998308c447
Govind-05 <govindbhardwaj501@gmail.com>
2023-06-07 00:51:52 +0530

    check bugs in cookie-v5

--- Server/index.js
+++ Server/index.js
@@ -52,7 +52,7 @@ app.post("/loginUser", async (req, res) => {
                     const user = { username: req.body.username }
                     const accessToken = jwt.sign(user, process.env.TOKEN_SECRET);
 
-                    res.cookie("username",req.body.username,{sameSite:"none",secure:true}).cookie("accessToken",accessToken,{httpOnly:true,secure:true,sameSite:"none"}).send({ msg: "User logged in"})
+                    res.cookie("username",req.body.username,{sameSite:"none",secure:true,domain:process.env.CLIENT_DOMAIN}).cookie("accessToken",accessToken,{httpOnly:true,secure:true,sameSite:"none",domain:process.env.CLIENT_DOMAIN}).send({ msg: "User logged in"})
                     
                 } else {
                     res.send({ msg: "error" })
